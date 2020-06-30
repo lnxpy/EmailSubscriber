b0220ede405785a16e69945d435ea56abe58662e
lnxpy <lnxpylnxpy@gmail.com>
2020-06-30 15:48:00 +0430

    task updated

--- core/tasks.py
+++ core/tasks.py
@@ -4,11 +4,12 @@ from core.models import Subscribers
 from celery import shared_task
 from time import sleep as slp
 from django.core.mail import send_mail
+from mysite.settings import EMAIL_HOST_USER
 
 @shared_task
 def add_email_to_subscribers(email):
     query = Subscribers.objects.create(email=email)
-    return '%s added to Subscribers model!' % (email)
+    return ['%s added to Subscribers model!' % (email), 'Ok']
 
 @shared_task
 def send_email_to_all(latency=0):
@@ -17,7 +18,7 @@ def send_email_to_all(latency=0):
     send_mail(
         'This is Title',
         'This is Body',
-        'alirezayahyapour80@gmail.com',
+        EMAIL_HOST_USER, # This is the sender email => Gets from settings.py
         [mail.email for mail in subscribers],
         fail_silently = False,
     )
