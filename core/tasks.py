548fb8aac07ae956c1f4a3f327aca341fbda7caa
lnxpy <lnxpylnxpy@gmail.com>
2020-07-01 17:27:07 +0430

    new task for flush created

--- core/tasks.py
+++ core/tasks.py
@@ -4,12 +4,11 @@ from core.models import Subscribers
 from celery import shared_task
 from time import sleep as slp
 from django.core.mail import send_mail
-from mysite.settings import EMAIL_HOST_USER
 
 @shared_task
 def add_email_to_subscribers(email):
     query = Subscribers.objects.create(email=email)
-    return ['%s added to Subscribers model!' % (email), 'Ok']
+    return '%s added to Subscribers model!' % (email)
 
 @shared_task
 def send_email_to_all(latency=0):
@@ -18,8 +17,13 @@ def send_email_to_all(latency=0):
     send_mail(
         'This is Title',
         'This is Body',
-        EMAIL_HOST_USER, # This is the sender email => Gets from settings.py
+        'alirezayahyapour80@gmail.com', # This is the sender email => Gets from settings.py
         [mail.email for mail in subscribers],
         fail_silently = False,
     )
     return 'I sent emails to all %s Subscribers.' % (subscribers.count())
+
+@shared_task
+def flush_database():
+    Subscribers.objects.all().delete()
+    return 'All subscribers records deleted!'
